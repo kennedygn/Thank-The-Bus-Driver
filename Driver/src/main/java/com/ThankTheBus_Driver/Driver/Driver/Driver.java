abf2fc0202455dcead50304c63fa345d0a823054
j_calca2 <j_calca2@uncg.edu>
2023-03-19 13:04:03 -0400

    Redoing everything

---
+++ Driver/src/main/java/com/ThankTheBus_Driver/Driver/Driver/Driver.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ThankTheBus_Driver.Driver.Driver;
+
+/**
+ *
+ * @author josephcalcagno
+ */
+public class Driver {
+     
+}
