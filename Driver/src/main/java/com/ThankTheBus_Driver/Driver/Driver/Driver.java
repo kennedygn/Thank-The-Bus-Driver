e131945f5b19ba3807383a491d67e0f8542fd54b
j_calca2 <j_calca2@uncg.edu>
2023-03-19 13:19:26 -0400

    Working on it

--- Driver/src/main/java/com/ThankTheBus_Driver/Driver/Driver/Driver.java
+++ Driver/src/main/java/com/ThankTheBus_Driver/Driver/Driver/Driver.java
@@ -4,10 +4,19 @@
  */
 package com.ThankTheBus_Driver.Driver.Driver;
 
+
+
 /**
  *
  * @author josephcalcagno
  */
 public class Driver {
+   private String name;
+   private int id;
+   
+   public Driver(String name, int id) {
+       this.name = name;
+       this.id = id;  
+   }
      
 }
