24849ae2ab4cfe2e7b9a74f56afff4c0b75543da
LualisonSr <111825827+LualisonSr@users.noreply.github.com>
2023-04-13 15:53:46 -0400

    finalprojskele

---
+++ TTBD_final/src/test/java/com/TTBD/TTBD_final/TtbdFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TTBD.TTBD_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtbdFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
