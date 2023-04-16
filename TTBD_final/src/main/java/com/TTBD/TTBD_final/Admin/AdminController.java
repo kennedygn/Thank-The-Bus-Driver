55ad3b1a94e50e545301847f7cd87de4e01cdd52
LualisonSr <111825827+LualisonSr@users.noreply.github.com>
2023-04-16 17:44:27 -0400

    Update AdminController.java

--- TTBD_final/src/main/java/com/TTBD/TTBD_final/Admin/AdminController.java
+++ TTBD_final/src/main/java/com/TTBD/TTBD_final/Admin/AdminController.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.TTBD.TTBD_final.Admin;
 
-/**
- *
- * @author kenne
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AdminController {
+    @Autowired
+    private AdminRepository adminRepository;
     
+    @GetMapping("/adminLogin")
+    public String showLoginForm() {
+        return "adminLogin";
+    }
+    @PostMapping("/adminAuth")
+    public String login(@RequestParam("admin_email") String adminEmail,
+                        @RequestParam("admin_password") String adminPassword) {
+        Admin admin = adminRepository.findByEmail(adminEmail);
+        if (admin != null && adminPassword.equals(admin.getAdminPassword())) {
+            return "/success";
+        } 
+        else {
+            return "/AdminLogin";
+        }
+    }
 }
