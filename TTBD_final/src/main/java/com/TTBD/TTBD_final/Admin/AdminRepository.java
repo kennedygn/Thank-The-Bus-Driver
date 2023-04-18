48aac01d2cafc3f1c296512744a2cdce62e72297
LualisonSr <111825827+LualisonSr@users.noreply.github.com>
2023-04-17 20:41:35 -0400

    Update AdminRepository.java

--- TTBD_final/src/main/java/com/TTBD/TTBD_final/Admin/AdminRepository.java
+++ TTBD_final/src/main/java/com/TTBD/TTBD_final/Admin/AdminRepository.java
@@ -2,7 +2,8 @@ package com.TTBD.TTBD_final.Admin;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
-public interface AdminRepository extends JpaRepository<Admin, String>{
-    Admin findByEmail(String adminEmail);
+public interface AdminRepository extends JpaRepository<Admin, String> {
+    Admin findByAdminEmail(String adminEmail);
 }
