ea125d5f89624a5c0c356e102d9b324d2508d772
Kennedy Ninh <122570019+kennedygn@users.noreply.github.com>
2023-04-18 01:41:54 -0400

    Update RiderController.java

--- src/main/java/com/ridercoder/ridercodereview/RiderController.java
+++ src/main/java/com/ridercoder/ridercodereview/RiderController.java
@@ -4,6 +4,7 @@
  */
 package com.ridercoder.ridercodereview;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -23,6 +25,11 @@ public class RiderController {
     @Autowired
     RiderService riderService;
 
+    @GetMapping("/home")
+    public String showHomePage() {
+        return "rider/rider-homepage";
+    }
+
     @GetMapping("/all")
     public String getAmount(Model model) {
         model.addAttribute("riderList", riderService.getBusFare());
@@ -30,12 +37,21 @@ public class RiderController {
 
     }
 
-    @GetMapping("/home")
-    public String homePage(Rider amount, Model model) {
-        model.addAttribute("riderList", riderService.getBusFare());
-        riderService.save(amount);
-        return "rider/rider-homepage";
+    @GetMapping("/balance")
+    public String homePage(Model model) {
+        double balance = riderService.getFareById(1).getFare();
+        model.addAttribute("fare", balance);
+        return "rider/rider-balance";
     }
+
+    /*@PostMapping("/save")
+    public String saveFare(@RequestParam("fare") double fare) {
+        Rider rider = riderService.getFareById(1);
+        rider.setFare(rider.getFare() + fare);
+        riderService.save(rider);
+        return "redirect:/rider/balance";
+    }*/
+
 
     /*@GetMapping("/routes")
     public String viewRoutes(Model model) {
@@ -44,14 +60,15 @@ public class RiderController {
         return "rider/rider-routes";
 
     }*/
-        
-    @PostMapping("/save")
-    public String saveFare(long id, double fare) {
-        Rider rider = riderService.getFareById(id);
-        double updatedFare = rider.getFare() + fare;
-        rider.setFare(updatedFare);
-        riderService.save(rider);
-        return "redirect:/rider/home";
-    }
+ @PostMapping("/save")
+public String saveFare(@RequestParam("id") long id, @RequestParam("fare") double fare, Model model) {
+    Rider rider = riderService.getFareById(id);
+    double updatedFare = rider.getFare() + fare;
+    rider.setFare(updatedFare);
+    riderService.save(rider);
+    model.addAttribute("fare", updatedFare); // Add updated balance to model
+    return "rider/rider-balance";
+}
+
 
 }
